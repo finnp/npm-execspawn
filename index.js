605e24ecde0fb8ded79b67a3b18912979699cb0f
Finn <derfinn@gmail.com>
2014-08-06 10:50:29 +0200

    Use spawn instead of execspawn to correctly handle quotes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var execspawn = require('execspawn')
+var spawn = require('child_process').spawn
 var parse = require('shell-quote').parse
 var xtend = require('xtend')
 var path = require('path')
@@ -26,10 +26,10 @@ module.exports = function(cmd, args, opts) {
   if (!opts) opts = {}
   if (!args) args = []
 
-  var parsed = parse(cmd, xtend(opts.env, args, {'':'$'})).map(toString).join(' ')
+  var parsed = parse(cmd, xtend(opts.env, args, {'':'$'})).map(toString)
   var env = opts.env || process.env
   var override = {}
   override[PATH_KEY] = npmRunPath(path.resolve(process.cwd(), opts.cwd || '.'), env[PATH_KEY] || process.env[PATH_KEY])
 
-  return execspawn(parsed, xtend(opts, {env:xtend(env, override)}))
+  return spawn(parsed[0], parsed.splice(1), xtend(opts, {env:xtend(env, override)}))
 }
